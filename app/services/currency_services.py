f974b115bc9b3e9a1b14028ae64efc8c2d44a51f
Olga1Ivanova <olga1ivanova@croc.ru>
2022-12-08 15:08:46 +0300

    Change post currency response

--- app/services/currency_services.py
+++ app/services/currency_services.py
@@ -7,13 +7,13 @@ from orm.models.currency import CurrencyModel
 from orm.schemas.currency import Currency
 
 
-def add_currency_for_date(date) -> str:
-    result = requests.post(
+def add_currency_for_date(date) -> dict:
+    requests.post(
         url=settings.FUNCTION_URL,
         data={"date": date},
         headers={"Content-Type": "application/json"},
     )
-    return result.json()
+    return {"message": "Record created"}
 
 
 def get_currency_rate_for_date(date, db=Depends()) -> list[Currency]:
